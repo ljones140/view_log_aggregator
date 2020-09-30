fe22b558037532d90b319ab2f2599e18a7d7b8f1
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2020-09-30 20:42:20 +0100

    Initial Commit
    - Setup Rspec
    - Rubocop with sensible defaults

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.disable_monkey_patching!
+  config.order = :random
+end
