55861f2e9698c06fbde5918ff7d4c5cd0c729ec6
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2020-10-02 20:07:58 +0100

    Duplicate pages increment view count
    Only one instance of PageEntry will exist per page name

--- lib/aggregate_builder.rb
+++ lib/aggregate_builder.rb
@@ -11,8 +11,11 @@ class AggregateBuilder
   end
 
   def add_entry(page_name:, visitor_ip:)
-    page_entry = PageEntry.new(name: page_name)
-    @page_views[page_name] = page_entry
+    page_entry = @page_views.fetch(page_name) {
+      new_entry = PageEntry.new(name: page_name)
+      @page_views[page_name] = new_entry
+      new_entry
+    }
     page_entry.add_view(ip: visitor_ip)
   end
 end
--- spec/aggregate_builder_spec.rb
+++ spec/aggregate_builder_spec.rb
@@ -16,5 +16,25 @@ RSpec.describe AggregateBuilder do
       aggregate_builder.add_entry(page_name: '/foo', visitor_ip: '192.168.0.1')
       expect(aggregate_builder.page_views['/foo'].views).to eq(1)
     end
+
+    context 'when adding same page multiple times' do
+      it 'does not create a new page entry' do
+        aggregate_builder = described_class.new
+        aggregate_builder.add_entry(page_name: '/foo', visitor_ip: '192.168.0.1')
+        expect {
+          aggregate_builder.add_entry(page_name: '/foo', visitor_ip: '192.168.0.1')
+        }.not_to change { aggregate_builder.page_views }
+      end
+
+      it 'does increment the page entries view count' do
+        aggregate_builder = described_class.new
+        aggregate_builder.add_entry(page_name: '/foo', visitor_ip: '192.168.0.1')
+        expect {
+          aggregate_builder.add_entry(page_name: '/foo', visitor_ip: '192.168.0.1')
+        }.to change { aggregate_builder.page_views['/foo'].views }
+          .from(1)
+          .to(2)
+      end
+    end
   end
 end
