0d35ad25c272b9697f92a2b377b0b7c9d6751fb1
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2020-10-03 16:18:26 +0100

    Refactor
    Pull out methods, hopefully improve readability

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -6,19 +6,29 @@ require './lib/page_view_aggregator'
 class LogParser
   def initialize(printer)
     @printer = printer
+    @aggregator = PageViewAggregator.new
   end
 
   def parse_log(file_path)
-    aggregator = PageViewAggregator.new
     File.readlines(file_path).each do |line|
       name, ip = line.split
-      aggregator.add_entry(page_name: name, visitor_ip: ip)
+      @aggregator.add_entry(page_name: name, visitor_ip: ip)
     end
-    aggregator.page_visits.sort_by(&:visits).reverse
-              .map { |entry| [entry.name, entry.visits] }
-              .then { |results| @printer.print_visits(results) }
-    aggregator.page_visits.sort_by(&:unique_views).reverse
-              .map { |entry| [entry.name, entry.unique_views] }
-              .then { |results| @printer.print_unique_views(results) }
+    print_page_visits(@aggregator.page_visits)
+    print_page_unique_views(@aggregator.page_visits)
+  end
+
+  private
+
+  def print_page_visits(visits)
+    visits.sort_by(&:visits).reverse
+          .map { |entry| [entry.name, entry.visits] }
+          .then { |results| @printer.print_visits(results) }
+  end
+
+  def print_page_unique_views(visits)
+    visits.sort_by(&:unique_views).reverse
+          .map { |entry| [entry.name, entry.unique_views] }
+          .then { |results| @printer.print_unique_views(results) }
   end
 end
